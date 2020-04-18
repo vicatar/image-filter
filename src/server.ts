c5b7efed4ac82d33989e5126d9f9a842d27e3743
Victor Lin <victor.s.lin@gmail.com>
2020-04-17 18:15:11 -0700

    Updated with endpoint /filteredimage

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -13,11 +13,10 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
-  //    1
+  //    
   //    1. validate the image_url query
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
@@ -28,8 +27,24 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+  app.get( "/filteredimage/", async ( req: Request, res: Response ) => {
+    let { image_url } = req.query;
 
-  //! END @TODO1
+    if ( !image_url ) {
+      return res.status(422)
+                .send(`image_url is required`);
+    }
+
+    const filteredpath = await filterImageFromURL(image_url);
+  
+    res.status(200)
+              .sendFile(filteredpath, function () {deleteLocalFiles([filteredpath])});
+    
+    console.log( `finsihed request & cleaned up` ); 
+    
+    return;
+  } );
+
   
   // Root Endpoint
   // Displays a simple message to the user
